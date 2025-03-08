03cebd7cd4b074d93ce5019614ce8e35625f3aba
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-03-08 12:06:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ menu = {
 print("Welcome to Pyrestaurant")
 #Print menu
 print("""
-  "Pizza":40,
-  "Pasta":50,
-  "Burger":60,
-  "Salad":70,
-  "Coffee":80,
+   Pizza:Rs-40
+   Pasta:Rs-50
+   Burger:Rs-60
+   Salad:Rs-70
+   Coffee:Rs-80
 """)
