60d3b7cfd8aec02d1f9dcfaa1648af8f092c9460
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-03-08 12:35:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,16 @@ menu = {
 st.write("Welcome to Pyrestaurant")
 #Print menu
 st.write("""
-   Pizza:Rs-40\n
-   Pasta:Rs-50\n
-   Burger:Rs-60\n
-   Salad:Rs-70\n
+   Pizza:Rs-40,\n
+   Pasta:Rs-50,\n
+   Burger:Rs-60,\n
+   Salad:Rs-70,\n
    Coffee:Rs-80
 """)
+# Order deta 
+order_total = 0
+#Get input order
+item_1 = text_input("Enter Your first order")
+if item_1 in menu:
+  order_total += menu[item_1]
+  st.write(f"Your item {item_1} has been added to yur order")
