7fb4a0bd1fc53e3fb0fe75e88f7da99cfd4ed1ad
Souporno Chakraborty <167761932+Tirthaboss@users.noreply.github.com>
2025-03-08 11:36:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+#A restaurant management system in python.
 
+#Difine the menu of restaurant 
+menu = {
+  "Pizza":40,
+  "Pasta":50,
+  "Burger":60,
+  "Salad":70,
+  "Coffee":80,
+}
+
+#Greet
+print("Welcome to Pyrestaurant")
+#Print menu
+print("""
+  "Pizza":40,
+  "Pasta":50,
+  "Burger":60,
+  "Salad":70,
+  "Coffee":80,
+""")
